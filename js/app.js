ffa2ef0ef135bfcffc84d0afb2935759bcc0fe82
unknown <mehedi15-2995@diu.edu.bd>
2022-08-31 22:26:12 +0600

    another

--- js/app.js
+++ js/app.js
@@ -46,7 +46,8 @@ const displayPhones = (phones,dataLimit)=>{
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
+          <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
+          
         </div>
       </div>
         
@@ -73,6 +74,14 @@ document.getElementById('btn-search').addEventListener('click',function(){
     processSearch(10);
     
 })
+//search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress',function(e){
+   
+    if(e.key==='Enter'){
+        processSearch(10);
+
+    }
+})
 
 
 const toggleSpinner = isLoading =>{
@@ -92,13 +101,31 @@ document.getElementById('btn-show-all').addEventListener('click',function(){
     processSearch();
 
 })
-const loadPhoneDetails= id =>{
+const loadPhoneDetails= async id =>{
     const url =` https://openapi.programming-hero.com/api/phone/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data);
+    displayPhoneDetails(data.data);
+}
+
+
+const displayPhoneDetails=(phone)=>{
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText= phone.name;
+    const phoneDetails  = document.getElementById('phone-details');
+    phoneDetails.innerHTML=`
+        <p>Realese Date:${phone.releaseDate ? phone.releaseDate: 'No Release Date Found'}</p>
+        <p>Storage:${phone.mainFeatures  ? phone.mainFeatures.storage:'No storage Information'}</p>
+        <p>Others:${phone.others ? phone.others.Bluetooth: 'No Bluetooth Information'}</p>
+    
+    
+    
+    `
+
 }
 
 
 
-// loadPhone();
+
+
+ loadPhone('apple');
